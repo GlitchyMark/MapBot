2a7c1d666dd196b0cf3f9504037020b54777e40b
MJ9001 <johnstonmd@vcu.edu>
2019-02-16 14:10:03 -0500

    Add project files.

---
+++ MapBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MapBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

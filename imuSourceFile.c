02e312733a26d012fde8cf2312fe82e3fea06b71
jvcu0 <46465611+jvcu0@users.noreply.github.com>
2019-04-08 11:54:23 -0400

    Add files via upload

---
+++ imuSourceFile.c
@@ -0,0 +1,15 @@
+//
+//  imuSourceFile.c
+//  
+//
+//  Created by Jamie Forsberg on 4/6/19.
+//
+
+//link this with the following: "-lwiringpi" with no quotes
+#include <wiringPiI2C.h>
+#include <stdio.h>
+
+i2cNum = 0x68;
+wiringPiI2C pi2c;
+
+
